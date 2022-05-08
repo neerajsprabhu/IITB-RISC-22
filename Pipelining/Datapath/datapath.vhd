5af7606dcba8d134474d068913d53e96af6e4868
Anubhav Bhatla <76862682+AnubhavBhatla@users.noreply.github.com>
2022-05-09 00:44:41 +0530

    More changes to the datapath

--- Pipelining/Datapath/datapath.vhd
+++ Pipelining/Datapath/datapath.vhd
@@ -94,12 +94,12 @@ architecture arch of datapath is
 		port(
 			clk : in std_logic;
 			wr_MEMWB : in std_logic;
-			MEMWB_PC, MEMWB_RF_D2, MEMWB_ALU_C, MEMWB_ALU2_C, MEMWB_DMem_D : in std_logic_vector(15 downto 0);
+			MEMWB_inc, MEMWB_PC, MEMWB_RF_D2, MEMWB_ALU_C, MEMWB_ALU2_C, MEMWB_DMem_D : in std_logic_vector(15 downto 0);
 			MEMWB_11_9, MEMWB_8_6, MEMWB_5_3, MEMWB_dec : in std_logic_vector(2 downto 0);
 			MEMWB_8_0 : in std_logic_vector(8 downto 0);
 			MEMWB_5_0 : in std_logic_vector(5 downto 0);
 			MEMWB_cy, MEMWB_z : in std_logic;
-			MEMWB_PC_Op, MEMWB_RF_D2_Op, MEMWB_ALU_C_Op, MEMWB_ALU2_C_Op, MEMWB_DMem_D_Op : out std_logic_vector(15 downto 0);
+			MEMWB_inc_Op, MEMWB_PC_Op, MEMWB_RF_D2_Op, MEMWB_ALU_C_Op, MEMWB_ALU2_C_Op, MEMWB_DMem_D_Op : out std_logic_vector(15 downto 0);
 			MEMWB_11_9_Op, MEMWB_8_6_Op, MEMWB_5_3_Op, MEMWB_dec_Op : out std_logic_vector(2 downto 0);
 			MEMWB_8_0_Op : out std_logic_vector(8 downto 0);
 			MEMWB_5_0_Op : out std_logic_vector(5 downto 0);
@@ -284,7 +284,7 @@ architecture arch of datapath is
   signal EXMEM_8_0_Op : std_logic_vector(8 downto 0);
   signal EXMEM_5_0_Op : std_logic_vector(5 downto 0);
   signal EXMEM_cy_Op, EXMEM_z_Op : std_logic;
-  signal MEMWB_PC_Op, MEMWB_RF_D2_Op, MEMWB_ALU_C_Op, MEMWB_ALU2_C_Op, MEMWB_DMem_D_Op : std_logic_vector(15 downto 0);
+  signal MEMWB_inc_Op, MEMWB_PC_Op, MEMWB_RF_D2_Op, MEMWB_ALU_C_Op, MEMWB_ALU2_C_Op, MEMWB_DMem_D_Op : std_logic_vector(15 downto 0);
   signal MEMWB_11_9_Op, MEMWB_8_6_Op, MEMWB_5_3_Op, MEMWB_dec_Op : std_logic_vector(2 downto 0);
   signal MEMWB_8_0_Op : std_logic_vector(8 downto 0);
   signal MEMWB_5_0_Op : std_logic_vector(5 downto 0);
@@ -399,7 +399,7 @@ Mux_DMem_Din: mux21 port map (A0=>EXMEM_RF_D1_Op, A1=>EXMEM_RF_D2_Op, S=>select_
 DMem: ram port map (wr=>wr_DMem, A=>DMem_A, Din=>DMem_Din, clk=>clk, Dout=>DMem_Dout);	
 
 --Mux_ALU2_A
-Mux_ALU2_A: mux21 port map (A0=>EXMEM_RF_D1_Op, A1=>EXMEM_inc_Op, S=>select_Mux_ALU2_A, Op=>ALU2_A);
+Mux_ALU2_A: mux21 port map (A0=>EXMEM_RF_D1_Op, A1=>EXMEM_PC_Op, S=>select_Mux_ALU2_A, Op=>ALU2_A);
 
 --Mux_ALU2_B
 Mux_ALU2_B: mux41 port map (A0=>"0000000000000001", A1=>EXMEM_SE6_Op, A2=>EXMEM_SE9_Op, A3=>"0000000000000000", S=>select_Mux_ALU2_B, Op=>ALU2_B);
@@ -418,6 +418,7 @@ MEM_WB: MEMWB port map (
 							MEMWB_DMem_D=>DMem_Dout, MEMWB_DMem_D_Op=>MEMWB_DMem_D_Op,
 							MEMWB_dec=>EXMEM_dec_Op, MEMWB_dec_Op=>MEMWB_dec_Op,
 							MEMWB_PC=>EXMEM_PC_Op, MEMWB_PC_Op=>MEMWB_PC_Op,
+							MEMWB_inc=>EXMEM_inc_Op, MEMWB_inc_Op=>MEMWB_inc_Op,
 							MEMWB_RF_D2=>EXMEM_RF_D2_Op, MEMWB_RF_D2_Op=>MEMWB_RF_D2_Op,
 							MEMWB_8_0=>EXMEM_8_0_Op, MEMWB_8_0_Op=>MEMWB_8_0_Op,
 							MEMWB_5_0=>EXMEM_5_0_Op, MEMWB_5_0_Op=>MEMWB_5_0_Op,
@@ -433,7 +434,7 @@ Mux_RF_A3: mux41_3 port map (A0=>MEMWB_5_3_Op, A1=>MEMWB_8_6_Op, A2=>MEMWB_11_9_
 S7: bit7shift port map (A=>MEMWB_8_0_Op, Op=>S7_Op);
 
 --Mux_RF-D3
-Mux_RF_D3: mux81 port map (A0=>MEMWB_ALU_C_Op, A1=>S7_Op, A2=>MEMWB_DMem_D_Op, A3=>MEMWB_PC_Op, A4=>MEMWB_ALU2_C_Op, A5=>"0000000000000000", A6=>"0000000000000000", A7=>"0000000000000000", S=>select_Mux_RF_D3, Op=>RF_D3);
+Mux_RF_D3: mux81 port map (A0=>MEMWB_ALU_C_Op, A1=>S7_Op, A2=>MEMWB_DMem_D_Op, A3=>MEMWB_inc_Op, A4=>MEMWB_ALU2_C_Op, A5=>"0000000000000000", A6=>"0000000000000000", A7=>"0000000000000000", S=>select_Mux_RF_D3, Op=>RF_D3);
 
 --Mux_jump_loc
 Mux_jump_loc: mux41 port map (A0=>inc_Op, A1=>MEMWB_ALU2_C_Op, A2=>MEMWB_RF_D2_Op, A3=>MEMWB_ALU_C_Op, S=>select_Mux_jump_loc, Op=>jump_loc);
